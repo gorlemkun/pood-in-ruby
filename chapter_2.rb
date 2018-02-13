3a3c37a0687baa19eb8fa0ab54b4969f6e12d3ab
gorlemkun <gorlemgorlem@gmail.com>
2018-02-14 02:24:36 +0900

    add SRP

--- chapter_2.rb
+++ chapter_2.rb
@@ -11,3 +11,23 @@ module A
     end
   end
 end
+
+module B
+  class Gear
+    attr_reader :chainring, :cog, :rim, :tire
+    def initialize(chainring, cog, rim, tire)
+      @chainring = chainring
+      @cog = cog
+      @rim = rim
+      @tire = tire
+    end
+
+    def ratio
+      chainring / cog.to_f
+    end
+
+    def gear_inches
+      ratio * (rim + tire * 2)
+    end
+  end
+end
